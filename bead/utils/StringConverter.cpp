3e166ba26c1e30a52e5b5c02de782998f1b5d496
kubs <mctest2334@gmail.com>
2021-09-06 02:14:02 +0200

    vec char to string

--- bead/utils/StringConverter.cpp
+++ bead/utils/StringConverter.cpp
@@ -5,3 +5,8 @@ std::wstring StringConverter::StringToWide(std::string str)
 	std::wstring wide_string(str.begin(), str.end());
 	return wide_string;
 }
+
+std::string StringConverter::StringFromCharVec(std::vector<char> src)
+{
+	return std::string(src.begin(), src.end());
+}
--- bead/utils/StringConverter.h
+++ bead/utils/StringConverter.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <string>
+#include <vector>
 
 class StringConverter
 {
 public:
 	static std::wstring StringToWide(std::string str);
+	static std::string StringFromCharVec(std::vector<char> src);
 };
